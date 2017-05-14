4111a176da15fc4175f5562c90d875f8c2691603
Wegman <leon.wegman@capgemini.com>
2017-05-14 13:59:09 +0200

    Initial commit

---
+++ src/main/java/com/leonwegman/homeapp/setup/HelloTester.java
@@ -0,0 +1,14 @@
+package com.leonwegman.homeapp.setup;
+
+/**
+ * Created by LWEGMAN on 14-5-2017.
+ */
+public class HelloTester {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i <= 10; i++) {
+            System.out.println("Hello for the " + i + " times.");
+        }
+    }
+}
